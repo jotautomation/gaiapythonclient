a714ce879390cc57e61adb30fdae12b070705feb
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2019-10-07 15:30:14 +0300

    Bump version
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gaiaclient",
-    version="0.0.23",
+    version="0.0.24",
     license='MIT License',
     author="JOT Automation Ltd.",
     author_email="rami.rahikkala@jotautomation.com",
