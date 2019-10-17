30a8c07e4dba070efc8078d14195eb3a8777bd81
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2019-10-17 15:04:39 +0300

    Example with only state handling - but it works
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 import time
+import json
 import gaiaclient
 
-client = gaiaclient.Client('localhost:1234')
+client = gaiaclient.Client('http://localhost:1234')
 
 # Get state of the tester
 print("State: " + client.state)
@@ -26,6 +27,8 @@ print(json.dumps(client.state_triggers, indent=4, sort_keys=True, cls=GaiaJsonEn
 while True:
     # From here starts the actual test sequence
 
+    client.applications['dut']['actions']['force-presence-on']()
+
     # Step 1: We are waiting that the test box gets ready and operator puts DUT(s) in
     # (Todo: implement waiting witht out polling)
     while not client.test_box_closing or client.ready_for_testing:
@@ -47,33 +50,29 @@ while True:
 
     # Execute the tests. Here's some examples.
 
-    # Change robot tool. Note! Tool change can be defined also in G-code
-    # but some time you can save time by changing the tool
-    # while doing something else.
-    client.applications["MainRobot"].actions["changeTo-AudioTool"]()
-
     # Run robot movement. See GcodeExample.GCode for g-code example and also how
     # to define tool on g-code.
     # Note that for safety reason when g-code is modified it will run once with low speed and power.
     # So if you made mistake and robot collides it won't brake anything
-    client.applications["MainRobot"].actions["cnc_run"](plain_text=GcodeExample.GCode)
+    # client.applications["MainRobot"]['actions']["cnc_run"](plain_text=GcodeExample.GCode)
 
     # Push button on DUT with pusher
-    client.applications["SideButtonPusher"].actions["Push"]()
+    # client.applications["SideButtonPusher"]['actions']["Push"]()
 
     # Optionally wait that pusher is on end position (detected by sensor)
-    client.applications["SideButtonPusher"].wait_state("Push")
+    # client.applications["SideButtonPusher"].wait_state("Push")
 
     # Release pusher
-    client.applications["SideButtonPusher"].actions["Release"]()
+    # client.applications["SideButtonPusher"]['actions']["Release"]()
 
     # Record audio
-    # (Not implemented in python client - yet!) client.applications["WaveRecorder"].actions["record-wave"](new Dictionary<string, object> { { "time_s", 2 }, { "filename", "testrecord.wav" } });
+    # (Not implemented in python client - yet!) client.applications["WaveRecorder"]['actions']["record-wave"](new Dictionary<string, object> { { "time_s", 2 }, { "filename", "testrecord.wav" } });
 
     # Play audio
-    # (Not implemented in python client - yet!) client.Applications["WavePlayer"].Actions["play-wave"](new Dictionary<string, object> { { "filename", "sine_1000Hz_-3dBFS_3s.wav" } });
-
+    # (Not implemented in python client - yet!) client.Applications["WavePlayer"]['actions']["play-wave"](new Dictionary<string, object> { { "filename", "sine_1000Hz_-3dBFS_3s.wav" } });
 
     # Step 4: Testing is ready and we release the DUT and give test result so that test box can indicate it to operator
-    # (Not implemented in python client - yet!) client.state_triggers["Release"](new Dictionary<string, object> { { "testResult1", "pass" }, { "testResult2", "pass" }, { "testResult3", "fail" } })
+    client.state_triggers["ReleasePass"]()
 
+    time.sleep(7)  # <-- DO NOT USE ON PRODUCTION!
+    client.applications['dut']['actions']['force-presence-off']()  # <-- DO NOT USE ON PRODUCTION!
