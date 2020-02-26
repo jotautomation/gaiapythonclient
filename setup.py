13d5c13cf391553e51f3874fe9ff04f66a23645f
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-02-26 08:27:37 +0200

    Fix requirement
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jotautomation/gaiapythonclient",
     py_modules=['gaiaclient'],
-    install_requires=['wheel', 'requests', 'websocket'],
+    install_requires=['wheel', 'requests', 'websocket-client'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
