a58534dc982c0b734dbd0b603b23a47efc351b88
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2021-01-11 13:34:35 +0200

    Reformat a bit
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- example.py
+++ example.py
@@ -13,12 +13,15 @@ print("State: " + CLIENT.state)
 
 # This is how you can download the recorded wave file from gaia machine.
 CLIENT.download_wave_file('sine_1000Hz_-3dBFS_3s.wav', 'sine_1000Hz_-3dBFS_3s.wav')
+
+# And this is how file is uploaded. The file can be later played on the machine.
+CLIENT.upload_wave_file('sine_1000Hz_-3dBFS_3s.wav')
+
+
 # This is how you get properties of application.
 # For example here we get current position of X-axle of main robot.
 print(CLIENT.applications['MainRobot']['properties']['position']['x'])
 
-# And this is how file is uploaded. The file can be later played on the machine.
-CLIENT.upload_wave_file('sine_1000Hz_-3dBFS_3s.wav')
 
 # Print available applications and actions
 class GaiaJsonEncoder(json.JSONEncoder):
