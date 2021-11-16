9607f201dcd798cd65966d082bb14d00bb7fa358
Atte Rankinen <atte.rankinen@jotautomation.com>
2021-11-16 05:13:13 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gaiaclient",
-    version="1.7.2",
+    version="1.7.3",
     license='MIT License',
     author="JOT Automation Ltd.",
     author_email="rami.rahikkala@jotautomation.com",
